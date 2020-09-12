7ddc9c5d74c8e0ab347a0b154a359eae9a7b59d8
Justin Vrana <justin.vrana@gmail.com>
2020-09-12 16:41:46 -0700

    depreciated

---
+++ poetry_export_hook/__init__.py
@@ -0,0 +1,7 @@
+import warnings
+
+warnings.warn(
+    "https://github.com/jvrana/poetry-export-hook is now depreciated. "
+    "Please use https://github.com/jvrana/poetry-hooks",
+    DeprecationWarning,
+)
